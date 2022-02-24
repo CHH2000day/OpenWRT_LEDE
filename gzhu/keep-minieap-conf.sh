a12b802809ee6196ac63360b7a29b83fcea66c4c
CHH2000day <chh1932383611@gmail.com>
2022-02-25 00:47:05 +0800

    更正sed参数

--- gzhu/keep-minieap-conf.sh
+++ gzhu/keep-minieap-conf.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #在非clean build中防止重复添加
-sed -n -e '/etc/minieap.conf' package/base-files/files/etc/sysupgrade.conf || sed -i  '/etc/minieap.conf' package/base-files/files/etc/sysupgrade.conf
+sed -n '/etc/minieap.conf' package/base-files/files/etc/sysupgrade.conf || sed -i  '/etc/minieap.conf' package/base-files/files/etc/sysupgrade.conf
