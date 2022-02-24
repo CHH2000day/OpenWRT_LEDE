f255cb0c9107f25b6a8ab8e521aea81b7cc2e753
CHH2000day <chh1932383611@gmail.com>
2022-02-25 00:52:40 +0800

    不使用sed避免转义问题

--- gzhu/keep-minieap-conf.sh
+++ gzhu/keep-minieap-conf.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #在非clean build中防止重复添加
-sed -n 'minieap.conf' package/base-files/files/etc/sysupgrade.conf || sed -i  '/etc/minieap.conf' package/base-files/files/etc/sysupgrade.conf
+sed -n 'minieap.conf' package/base-files/files/etc/sysupgrade.conf || printf '\n/etc/minieap.conf' >> package/base-files/files/etc/sysupgrade.conf
