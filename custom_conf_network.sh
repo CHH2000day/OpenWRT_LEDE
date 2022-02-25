721ca748e4000602088b0d24175b900da9c8838b
CHH2000day <chh1932383611@gmail.com>
2022-02-25 11:10:31 +0800

    更正sed

--- custom_conf_network.sh
+++ custom_conf_network.sh
@@ -3,4 +3,4 @@
 sed -i 's/192.168.1.1/192.168.106.1/g' package/base-files/files/bin/config_generate
 
 #修正连接数（by ベ七秒鱼ベ）(modified by CHH2000day)
-sed -n  'net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf || sed -i  '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
+sed -n -e '/net.netfilter.nf_conntrack_max/p' package/base-files/files/etc/sysctl.conf || sed -i  '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
